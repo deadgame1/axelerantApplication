0745039e1f0ad9f339ba783b42aa295451ff4b3b
Aditya <aditya.shanker@getastra.com>
2021-11-08 01:18:11 +0530

    login controller, entties

---
+++ src/Controller/FeedController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FeedController extends AbstractController
+{
+    /**
+     * @Route("/feed", name="feed")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/FeedController.php',
+        ]);
+    }
+}
